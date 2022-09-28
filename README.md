fec38257b8044250ea84e24e8d71b20a513f19ce
Luz Angelique Madrazo <luzm321@yahoo.com>
2022-09-28 15:49:30 -0500

    added tax calculation methods/logic

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Problem Description:
+Sales Taxes Problem Description:
 
 There are a variety of items for sale at a store. When a customer purchases items, they receive a receipt. The receipt
 lists all of the items purchased, the sales price of each item (with taxes included), the total sales taxes for all items,
@@ -13,37 +13,37 @@ all taxes and totals correctly. When calculating the sales tax, round the value 
 a taxable item costs $5.60, an exact 10% tax would be $0.56, and the final price after adding the rounded tax of $0.60
 should be $6.20.
 
-INPUT 1
+INPUT 1:
 1 Book at 12.49
 1 Book at 12.49
 1 Music CD at 14.99
 1 Chocolate bar at 0.85
 
-OUTPUT 1
+OUTPUT 1:
 Book: 24.98 (2 @ 12.49)
 Music CD: 16.49
 Chocolate bar: 0.85
 Sales Taxes: 1.50
 Total: 42.32
 
-INPUT 2
+INPUT 2:
 1 Imported box of chocolates at 10.00
 1 Imported bottle of perfume at 47.50
 
-OUTPUT 2
+OUTPUT 2:
 Imported box of chocolates: 10.50
 Imported bottle of perfume: 54.65
 Sales Taxes: 7.65
 Total: 65.15
 
-INPUT 3
+INPUT 3:
 1 Imported bottle of perfume at 27.99
 1 Bottle of perfume at 18.99
 1 Packet of headache pills at 9.75
 1 Imported box of chocolates at 11.25
 1 Imported box of chocolates at 11.25
 
-OUTPUT 3
+OUTPUT 3:
 Imported bottle of perfume: 32.19
 Bottle of perfume: 20.89
 Packet of headache pills: 9.75
--- salesMain.js
+++ salesMain.js
@@ -54,6 +54,76 @@ const getItemSelectedFromDatabase = (itemName) => {
     });
 };
 
+//converts string format to number format of product price:
+const convertFromCurrency = (numberInStringForm) => {
+    let newNumber;
+    if (numberInStringForm.toString().includes('$')) {
+        newNumber = numberInStringForm.replace('$', '');
+    } else {
+        newNumber = numberInStringForm;
+    }
+    // This line is in case numbers with a comma are passed. The toString() is there in case theres a number passed and not a string.
+    // Without this line the code will break, because you cannot do a replace on a number.
+    newNumber = parseFloat(newNumber.toString().replace(/,/g, ''));
+    return newNumber;
+};
+
+//TAX CALCULATION METHODS:
+const calculateImportTax = (product) => {
+    let totalPriceAfterImportTax = 0;
+    let importTax = 0;
+    let itemTax = .05
+    let productPrice;
+
+    productPrice = convertFromCurrency(product.price);
+
+    if(product.import) {
+        //Round up then add to original price to get the total with tax.
+        let calculatedPriceAfter5PercentTax = (productPrice * itemTax) + productPrice;
+        //Reference: https://stackoverflow.com/questions/10413573/rounding-up-to-the-nearest-0-05-in-javascript
+        let roundedToNearest5Cent = (Math.ceil(calculatedPriceAfter5PercentTax*20)/20);
+        totalPriceAfterImportTax += roundedToNearest5Cent;
+        importTax = totalPriceAfterImportTax - productPrice;
+    } else {
+        totalPriceAfterImportTax += productPrice;
+    }
+    return {totalPriceAfterImportTax: totalPriceAfterImportTax, importTax: importTax};
+};
+
+const calculateSalesTax = (product) => {
+    //This calculates the total amount you paid after sales taxes with exception of product types in conditions below:
+    let totalPriceAfterSalesTax = 0;
+    let salesTax = 0;
+    let itemTax = .10;
+    if (product.type !== "Food" && product.type !== "Medical" && product.type !== "Book") {
+        let productPrice = convertFromCurrency(product.price);
+        let calculatedPriceAfter10PercentTax = (productPrice * itemTax) + productPrice;
+        // This rounds up to the nearest 5 cents:
+        let roundedToNearest5Cent = (Math.ceil(calculatedPriceAfter10PercentTax*20)/20)
+        totalPriceAfterSalesTax += roundedToNearest5Cent;
+        salesTax = totalPriceAfterSalesTax - productPrice;
+    }
+    return {totalPriceAfterSalesTax: totalPriceAfterSalesTax, salesTax: salesTax};
+};
+
+//method to calc both import & sales taxes of product
+const calculateTaxesOfItem = (productToTax) => {
+    let productPrice = convertFromCurrency(productToTax.price)
+    let importTaxCalculationResults = calculateImportTax(productToTax);
+    let salesTaxCalculationResults = calculateSalesTax(productToTax);
+    console.log('sales tax', salesTaxCalculationResults, importTaxCalculationResults);
+    let importTax = importTaxCalculationResults.importTax;
+    let salesTax = salesTaxCalculationResults.salesTax;
+    let itemPriceAfterAllTaxes =  (importTax + salesTax) + productPrice
+    let addedTax = importTax + salesTax;
+    return {
+        addedTax: addedTax,
+        importTax: importTax,
+        salesTax: salesTax,
+        priceTotalAfterTax: itemPriceAfterAllTaxes
+    };
+}
+
 //method returns html representation of store products:
 const renderPage = () => {
     return `
